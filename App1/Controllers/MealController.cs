b9db5d223cb29d51dfd62a6bab033661f71671bd
asekito <dresekito@gmail.com>
2021-01-09 16:12:41 -0700

    yas

---
+++ App1/Controllers/MealController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data.SQLite;
+using Microsoft.AspNetCore.Mvc;
+using App1.Models;
+
+namespace App1.Controllers
+{
+    public class MealController : Controller
+    {
+        public IActionResult AddMeal()
+        {
+            return View();
+        }
+
+        public IActionResult MealList()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Meal meal)
+        {
+            try
+            {
+                string cs = @"Data Source=/Users/andrea/Projects/Database/App1Db.db";
+
+                using var connect = new SQLiteConnection(cs);
+                //using var connect = new SQLiteConnection("Data Source=App1Db.db;");
+                connect.Open();
+
+                using var cmd = new SQLiteCommand(connect);
+                cmd.CommandText = "INSERT INTO Meals(title, rating, location, dateEaten, comments) VALUES(@title, @rating, @location, @dateEaten, @comments)";
+
+                cmd.Parameters.AddWithValue("@title", meal.MealTitle);
+                cmd.Parameters.AddWithValue("@rating", meal.Rating);
+                cmd.Parameters.AddWithValue("@location", meal.Location);
+                cmd.Parameters.AddWithValue("@dateEaten", meal.DateEaten);
+                cmd.Parameters.AddWithValue("@comments", meal.Comments);
+                cmd.Prepare();
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("row inserted");
+
+                return Redirect("MealList");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error caught here!!!");
+                Console.WriteLine(e);
+                return Redirect("AddMeal");
+            }
+        }
+
+    }
+}
---
+++ App1/Models/Meal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace App1.Models
+{
+    public class Meal
+    {
+        //public int id { get; set; }
+        public string MealTitle { get; set; }
+        public int Rating { get; set; }
+        public string Location { get; set; }
+        public DateTime DateEaten { get; set; }
+        public string Comments { get; set; }
+    }
+}
