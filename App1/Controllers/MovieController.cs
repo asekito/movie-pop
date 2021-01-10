2cdcba845d2a8520c43c0a9ed4b71b0ff91c4f46
asekito <dresekito@gmail.com>
2021-01-10 10:23:48 -0700

    shifting

---
+++ App1/Controllers/MovieController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data.SQLite;
+using Microsoft.AspNetCore.Mvc;
+using App1.Models;
+
+namespace App1.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult AddMovie()
+        {
+            try
+            {
+                string cs = @"Data Source=/Users/andrea/Projects/Database/App1Db.db";
+
+                using var connect = new SQLiteConnection(cs);
+                //using var connect = new SQLiteConnection("Data Source=App1Db.db;");
+                connect.Open();
+
+                string data = "SELECT * FROM Meals";
+                using var cmd = new SQLiteCommand(data, connect);
+                using SQLiteDataReader rdr = cmd.ExecuteReader();
+
+                // need to finish this and handle the data and send to view 
+            }
+            catch (Exception e)
+            {
+                return View(e);
+            }
+            return View();
+        }
+
+        public IActionResult MovieList()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            try
+            {
+                string cs = @"Data Source=/Users/andrea/Projects/Database/App1Db.db";
+
+                using var connect = new SQLiteConnection(cs);
+                //using var connect = new SQLiteConnection("Data Source=App1Db.db;");
+                connect.Open();
+
+                using var cmd = new SQLiteCommand(connect);
+                cmd.CommandText = "INSERT INTO Movies(Title, Director, ReleaseDate, Genre, Duration, Summary) VALUES(@title, @director, @releaseDate, @genre, @duration, @summary)";
+
+                cmd.Parameters.AddWithValue("@title", movie.title);
+                cmd.Parameters.AddWithValue("@director", movie.director);
+                cmd.Parameters.AddWithValue("@releaseDate", movie.releaseDate);
+                cmd.Parameters.AddWithValue("@genre", movie.genre);
+                cmd.Parameters.AddWithValue("@duration", movie.duration);
+                cmd.Parameters.AddWithValue("@summary", movie.summary);
+                cmd.Prepare();
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("row inserted");
+
+                return Redirect("MovieList");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error caught here!!!");
+                Console.WriteLine(e);
+                return Redirect("AddMovie");
+            }
+        }
+
+    }
+}
---
+++ App1/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace App1.Models
+{
+    public class Movie
+    {
+        //public int id { get; set; }
+        public string title { get; set; }
+        public string director { get; set; }
+        public DateTime releaseDate { get; set; }
+        public List<string> genre { get; set; }
+        public string duration { get; set; }
+        public string summary { get; set; }
+    }
+}
